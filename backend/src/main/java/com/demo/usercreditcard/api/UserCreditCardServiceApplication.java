e57c9baca2403c347eba1ed6069e3f448a741c6b
Solomon-Alexander <solomonprathap@gmail.com>
2018-06-05 12:10:36 +1000

    Create UserCreditCardServiceApplication.java

---
+++ backend/src/main/java/com/demo/usercreditcard/api/UserCreditCardServiceApplication.java
@@ -0,0 +1,12 @@
+package com.demo.usercreditcard.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCreditCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCreditCardServiceApplication.class, args);
+	}
+}
